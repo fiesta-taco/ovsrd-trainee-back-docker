f96708ce0235c3bca1c95fcfd02f2e3abe504181
ihorbilash <first2007.ib@gmail.com>
2023-08-28 17:25:09 +0300

    changes 28/08

--- src/config/aws.config.ts
+++ src/config/aws.config.ts
@@ -4,8 +4,6 @@ dotenv.config({path:`.env`})
 
 
 AWS.config.update({
-  accessKeyId: process.env.AWSAccessKeyId,
-  secretAccessKey: process.env.AWSSecretKey,
   region: process.env.AWSRegion,
 });
 
