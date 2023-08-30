7f2cc3eab71bddb9e9b7f63d00a7fcbf2850f500
ihorbilash <95218369+ihorbilash@users.noreply.github.com>
2023-08-30 14:15:50 +0300

    Add image functionality (#4)

--- src/api/image/image.router.ts
+++ src/api/image/image.router.ts
@@ -3,3 +3,4 @@ import imageController from "./image.controller";
 export const imageRouter = express.Router();
 
 imageRouter.route('/upload').post(imageController.uploadImage);
+imageRouter.route('/get').post(imageController.getImage);
--- src/api/image/image.service.ts
+++ src/api/image/image.service.ts
@@ -1,4 +1,4 @@
-import s3, { putObjectParam } from '../../config/aws.config';
+import s3, { putObjectParam,getObjectParam } from '../../config/aws.config';
 
 
 
@@ -8,6 +8,17 @@ export default {
     try {
       putObjectParam.Key = fileKey;
       const url = await s3.getSignedUrlPromise('putObject', putObjectParam);
+      return url;
+    } catch (error) {
+      console.error('Error:', error);
+      throw error;
+    }
+  },
+
+  async getImageFromS3(fileKey: string) {
+    try {
+      getObjectParam.Key = fileKey;
+      const url = await s3.getSignedUrlPromise('getObject', getObjectParam);
       return url;
     } catch (error) {
       console.error('Error:', error);
